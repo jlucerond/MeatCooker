f7678a65149229b1fd70e27814e125e317d825d8
Joe Lucero <jlucerond@gmail.com>
2015-08-31 15:18:25 -0400

    Initial Commit

---
+++ MeatCooker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeatCooker
+//
+//  Created by Joe Lucero on 8/31/15.
+//  Copyright (c) 2015 Joe Lucero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
